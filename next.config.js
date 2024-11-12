88e9bf6f327af725d0307d14d0382be29e5297a6
LourenaOhara <lourena6ohara@gmail.com>
2024-11-12 16:10:52 -0300

    atualizar next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
+}
+
+module.exports = {
+  assetPrefix: './',
+  basePath: '/portifolio',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
